42d5354f7764c7918e307b7d7ca6a5410054a4da
patelll22 <honeypatel424@gmail.com>
2025-07-16 17:12:19 +0530

    working

---
+++ script.js
@@ -0,0 +1,34 @@
+const imageMap = {
+  tourism: 'images/travel.jpg' ,
+  corporate: 'images/corporate.jpg',
+  student: 'images/students (3).jpg',
+  working: 'images/professionals.jpg',
+  backpacker: 'images/students.jpg',
+  medical: 'images/medical.jpg',
+  event: 'images/wedding.jpg',
+  construction: 'images/worker.jpg',
+  airport: 'images/air.jpg',
+  relocation: 'images/rent.jpg',
+};
+
+
+
+function showImage(sector) {
+  const display = document.getElementById('displayArea');
+  display.innerHTML = `
+    <div class="image-container">
+      <img src="${imageMap[sector]}" alt="${sector} Image">
+    </div>
+  `;
+}
+
+
+
+function toggleDropdown() {
+  const menu = document.getElementById('dropdownContent');
+  menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+}
+
+
+
+
